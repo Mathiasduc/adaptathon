c3be3a52ff7b73ca22afcf98b6eac3ea947790a2
Mathias Ducancel <mathiasduc@gmail.com>
2016-11-18 00:06:07 +0100

    Add the buttons for next questions and animations

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 (function () {
 	var app = {
 
-		buttonSelect: document.getElementsByTagName('button'), 
+		buttonSelect: $("button"), 
+		i: 0,
 
 		init: function(){
 			this.listeners();
 		},
 
 		listeners: function(){
-			buttonSelect.on('click', clickButton);
+			const me = this;
+			me.buttonSelect.on('click', me.clickButton.bind(me));
 		},
 
 		clickButton: function(event){
-			console.log("click");
+			var me = this;
+			$("#question").addClass('animated fadeOut');
+			window.setTimeout(next,500);
+
+			function next(){
+			$("#question").removeClass('animated fadeOut');
+			$("#question").text(me.questions[me.i]).addClass("animated fadeIn");
+			me.i++;
+			}
+			
 		},
+
+		questions: [
+		"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+		"Vero dolores deleniti consectetur beatae odit. Laboriosam, impedit minus perferendis sit,",
+		"eaque quae praesentium ipsum deleniti commodi maiores, autem magnam iusto. Eos.",
+		"question 3",
+		"question 4",
+		"question 5",
+		"question 6",
+		"question 7"
+		]
+
+
 	};
 
 	app.init();
