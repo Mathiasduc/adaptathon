24aa3ea3d511f83d58b4242f2bd0149fb5c56b02
Mathias Ducancel <mathiasduc@gmail.com>
2016-11-18 08:53:00 +0100

    Add the display of answers

--- app.js
+++ app.js
@@ -19,11 +19,19 @@
 			window.setTimeout(next,500);
 
 			function next(){
-			$("#question").removeClass('animated fadeOut');
-			$("#question").text(me.questions[me.i]).addClass("animated fadeIn");
-			me.i++;
+				$("#question").removeClass('animated fadeOut');
+				$("#question").text(me.questions[me.i]).addClass("animated fadeIn");
+				me.i++;
 			}
-			
+			if(!me.questions[me.i]){
+				me.answerDisplay();
+			}
+		},
+
+		answerDisplay: function(){
+			$("#questioningBox").hide();
+			$("#answer").html(this.answer);
+			$("#answer2").html(this.answer2);
 		},
 
 		questions: [
@@ -35,9 +43,18 @@
 		"question 5",
 		"question 6",
 		"question 7"
-		]
+		],
 
+		answer: "Vous etes une entreprise de type truc et vous pouvez beneficier de ce "+
+		"<a class='link' href='quelquepart' data-content='info concernant ce lien'>lien vers</a>"+
+		"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consectetur atque fugiat,"+
+		" accusamus illum voluptatem nam deleniti sequi debitis itaque doloribus esse, dolores perferendis"+
+		" at! Inventore, a placeat aliquid quam!",
 
+		answer2: "Votre situation vous rend potentielenment eligible pour tels financement:<br>" +
+		'<img src="http://www.cocerto.fr/wp/wp-content/uploads/2015/10/Agefiph_aides_montants_embauche_handicapes_2016.png"'+
+		' alt="liste des financements possibles" /> '
+		
 	};
 
 	app.init();
