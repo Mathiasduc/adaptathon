929af7990c8b9ec3407900c7905a714465db0c21
Mathias Ducancel <mathiasduc@gmail.com>
2016-11-18 15:15:06 +0100

    Add solutions section and some (poor) styling

--- app.js
+++ app.js
@@ -16,11 +16,13 @@
 		clickButton: function(event){
 			var me = this;
 			$("#question").addClass('animated fadeOut');
-			window.setTimeout(next,500);
+			window.setTimeout(next,	110);
 
 			function next(){
 				$("#question").removeClass('animated fadeOut');
 				$("#question").text(me.questions[me.i]).addClass("animated fadeIn");
+				$(".blue").text("Non");
+				$(".orange").text("Oui");
 				me.i++;
 			}
 			if(!me.questions[me.i]){
@@ -31,29 +33,35 @@
 		answerDisplay: function(){
 			$("#questioningBox").hide();
 			$("#answer").html(this.answer);
-			$("#answer2").html(this.answer2);
+			/*$("#answer2").html(this.answer2);*/
+			$(".link").popup();
 		},
 
 		questions: [
-		"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-		"Vero dolores deleniti consectetur beatae odit. Laboriosam, impedit minus perferendis sit,",
-		"eaque quae praesentium ipsum deleniti commodi maiores, autem magnam iusto. Eos.",
-		"question 3",
-		"question 4",
-		"question 5",
-		"question 6",
-		"question 7"
+		"Etape 2: Disposez d'un accord d'entreprise?",
+		"Etape 3: Etes vous une entreprise du secteur sanitaire et social ?",
+		"Etape 4: Le salarié est-il bénéficiaire de la loi 2015?",
+		"Etape 5: La reconnaissance RQTH est-elle en cours de validité?",
+		"Etape 6: Le salarié a-t-il des restrictions d'aptitudes?"
 		],
 
-		answer: "Vous etes une entreprise de type truc et vous pouvez beneficier de ce "+
-		"<a class='link' href='quelquepart' data-content='info concernant ce lien'>lien vers</a>"+
-		"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consectetur atque fugiat,"+
-		" accusamus illum voluptatem nam deleniti sequi debitis itaque doloribus esse, dolores perferendis"+
-		" at! Inventore, a placeat aliquid quam!",
+		answer: 
+		"<h1 class='answers'>Votre solution: </h1>" + 
+		"<div class='answers' >Vous etes une entreprise privée ne ne bénéficiant pas d'un accord d'entreprise et n'appartenant " +
+		"pas au secteur sanitaire et social.</div>"+
+		"<div class='answers' >Vous avez declaré un salarie titulaire de la RQTH.</div>" +
+		"<div class='answers' >Le salarie presente des contre-indications liées à son poste de travail posées par la médecine du travail.</div>"+
+		"<div class='answers' >Aide au maintien dans l'emploi:</div>"+
+		"<div class='answers' >Vous pouvez prétendre à une aide de 2000 Euros deblocable rapidement afin de subvenir à l'adaptation du poste de travail"+
+		"<div class='answers' >En fontion de vos besoin une aide complémentaire de 3000 Euros peut être débloqué</div>"+
+		"<div class='answers' >Contacter un conseiller SAMETH 31</div>"+
+		"<div class='answers' >05 67 69 01 36</div>"+
+		"<div class='answers' >info@sameth31.fr</div>"+
+		"<div class='answers' >Prise en charge sous 48h</div>"+
+		"<div class='answers' ><button class='ui button'>Envoyer un mail type</button></di>",
+		
 
-		answer2: "Votre situation vous rend potentielenment eligible pour tels financement:<br>" +
-		'<img src="http://www.cocerto.fr/wp/wp-content/uploads/2015/10/Agefiph_aides_montants_embauche_handicapes_2016.png"'+
-		' alt="liste des financements possibles" /> '
+
 		
 	};
 
